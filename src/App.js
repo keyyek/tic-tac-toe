3ad56034b3fe4db4e35e083236727b04309765f4
nadav <235nadav@gmail.com>
2022-11-22 00:08:30 +0200

    board and winner have been added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { Board } from "./components/Board";
+import { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 2],
+    [2, 4, 6],
+  ];
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying === true ? "X" : "O";
+      } else {
+        return value;
+      }
+    });
+    checkWinner(updatedBoard);
+    setBoard(updatedBoard);
+    setXPlaying(!xPlaying);
+  };
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        console.log(board[x]);
+        return board[x];
+      }
+    }
+  };
+  // const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
   return (
     <div className="App">
-      Hello
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 }
