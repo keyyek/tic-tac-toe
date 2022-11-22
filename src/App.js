8e8e23e21725d3d795152f07a90b093f68a4cada
nadav <235nadav@gmail.com>
2022-11-22 18:38:27 +0200

    1)game work 2)add reset button 3)add results board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Board } from "./components/Board";
 import { useState } from "react";
-
+import { ScoreBoard } from "./components/ScoreBoard";
+import { ResetButton } from "./components/ResetButton";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
   ];
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  const [gameOver, setGameOver] = useState(false);
+
   const handleBoxClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
       if (idx === boxIdx) {
@@ -24,7 +28,22 @@ function App() {
         return value;
       }
     });
-    checkWinner(updatedBoard);
+    const winner = checkWinner(updatedBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore });
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore });
+      }
+    }
+
+    // console.log(scores);
+
     setBoard(updatedBoard);
     setXPlaying(!xPlaying);
   };
@@ -33,15 +52,21 @@ function App() {
     for (let i = 0; i < WIN_CONDITIONS.length; i++) {
       const [x, y, z] = WIN_CONDITIONS[i];
       if (board[x] && board[x] === board[y] && board[y] === board[z]) {
-        console.log(board[x]);
+        setGameOver(true);
         return board[x];
       }
     }
   };
+  const resetBoard = () => {
+    setGameOver(false);
+    setBoard(Array(9).fill(null));
+  };
   // const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
   return (
     <div className="App">
-      <Board board={board} onClick={handleBoxClick} />
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
+      <ResetButton resetBoard={resetBoard} />
     </div>
   );
 }
